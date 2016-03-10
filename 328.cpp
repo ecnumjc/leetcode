da9d0d44ae59e656f2a41ea394c210705bcc89a8
ecnumjc <593074398@qq.com>
2016-03-10 22:53:34 +0800

    328
    328

--- 328.cpp
+++ 328.cpp
@@ -1,5 +1,3 @@
-/**
-* 328   Definition for singly-linked list.*/
 struct ListNode {
 	int val;
 	ListNode *next;
@@ -9,8 +7,21 @@ struct ListNode {
 class Solution {
 public:
 	ListNode* oddEvenList(ListNode* head) {
+		if (!head)
+			return head;
+		ListNode *p, *q, *temp;
+		p = head;
+		q = p->next;
+		temp = q;
+		while (q != NULL && q->next != NULL)
+		{
+			p->next = q->next;
+			p = p->next;
+			q->next = p->next;
+			q = q->next;
+		}
+		p->next = temp;
+		return head;
 
-	
-	
 	}
 };
