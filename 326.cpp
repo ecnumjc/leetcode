fa42d463b5ef52886dced72ce139ed14308c879f
ecnumjc <593074398@qq.com>
2016-03-21 16:30:57 +0800

    326
    326

---
+++ 326.cpp
@@ -0,0 +1,24 @@
+
+//326. Power of Three
+class Solution {
+public:
+	bool isPowerOfThree(int n)
+	{
+		if (n==0)
+		{
+			return false;
+		}
+		while (n % 3 == 0)
+		{
+			n = n / 3;
+		}
+		if ( n == 1 )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
